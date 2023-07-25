205fb894691d9e7a3c79ab7d42514aa0ee8c74a6
Gasak Ruslan <rusya.gasak@mail.ru>
2023-07-25 13:07:49 +0300

    Мой диплом на тему: Сайт для помощи поиска работы

---
+++ job_search_help_site/search_site/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class SearchSiteConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'search_site'
+    verbose_name = "Сайт поиска работы"
