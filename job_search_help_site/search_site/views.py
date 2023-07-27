fd514a6f65f312c03c163a5ebb55e3de70d64860
Gasak Ruslan <rusya.gasak@mail.ru>
2023-07-27 20:40:04 +0300

    Перенс логику в index.html

--- job_search_help_site/search_site/views.py
+++ job_search_help_site/search_site/views.py
@@ -5,9 +5,5 @@ def index(request):
     return render(request, "index.html")
 
 
-def rb(request):
-    return render(request, "rb.html")
-
-
 def help_for_people(request):
     return render(request, "help.html")
