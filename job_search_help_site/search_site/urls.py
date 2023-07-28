f6d710596baab23cd53786095b3d46ffaf2abc2c
Gasak Ruslan <rusya.gasak@mail.ru>
2023-07-28 19:08:14 +0300

    Добавил csrf_token немного отредачил html

--- job_search_help_site/search_site/urls.py
+++ job_search_help_site/search_site/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("help/", views.help_for_people, name="help")
+    path("help/", views.help_for_people, name="help"),
 ]
