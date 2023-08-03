362dc54fe29df8976120c15c0372fb85e01f17fc
Gasak Ruslan <rusya.gasak@mail.ru>
2023-08-03 14:18:15 +0300

    в настройках поменял AUTH_USER_MODEL

--- job_search_help_site/job_search_help_site/settings.py
+++ job_search_help_site/job_search_help_site/settings.py
@@ -24,7 +24,8 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # DB
-AUTH_USER_MODEL = 'search_site.MyUser'  # ВОТ ТУТ ВОТ ЕСЛИ ЧТО ИЗМЕНИТЬ!!!!!!!!!
+# AUTH_USER_MODEL = 'search_site.MyUser'  # ВОТ ТУТ ВОТ ЕСЛИ ЧТО ИЗМЕНИТЬ!!!!!!!!!
+AUTH_USER_MODEL = 'search_site.CustomUser'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
