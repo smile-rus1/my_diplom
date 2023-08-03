3cc324ebd2b522aea3f692aabe9661f8d06aef74
Gasak Ruslan <rusya.gasak@mail.ru>
2023-08-03 14:16:17 +0300

    в админке зарегал модель пользователя

--- job_search_help_site/search_site/admin.py
+++ job_search_help_site/search_site/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from search_site.models import CustomUser
+
+
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ("email", "is_active", "is_staff")
+    list_filter = ("is_active", "is_staff")
+    search_fields = ("email", )
+
+
+admin.register(CustomUser, CustomUserAdmin)
