225662f09b3fb0a9e07a51a3aa3eab32ca6c5907
Gasak Ruslan <rusya.gasak@mail.ru>
2023-08-03 19:18:21 +0300

    подправил название сервиса и немного сделал по другому авторизацию и регистрацию

---
+++ job_search_help_site/search_site/services/auth.py
@@ -0,0 +1,54 @@
+from django.contrib import messages
+from django.contrib.auth import login, authenticate
+
+from search_site.models import CustomUser
+
+
+def register_applicant(request, email: str, password1: str, password2: str):
+    if not _match_password(password1, password2):
+        return messages.error(request, 'Пароли не совпадают')
+
+    if not _is_valid_password(password1):
+        return messages.error(request, "Пароль должен состоять из 8 символов и в нем должны быть буквы!")
+
+    if not _is_not_exists_email(email):
+        return messages.error(request, "Такой email уже зарегистрирован!")
+
+    user = CustomUser(email=email)
+    user.set_password(password1)
+    user.save()
+
+    login_user(request, {"email": email, "password": password1})
+    return True
+
+
+def login_user(request, user: dict):
+    user = authenticate(request, email=user["email"], password=user["password"])
+    if user is not None:
+        login(request, user)
+    else:
+        return False
+
+    return True
+
+
+def _match_password(password1: str, password2: str):
+    if password1 != password2:
+        return False
+    return True
+
+
+def _is_valid_password(password1: str):
+    if len(password1) < 8:
+        return False
+
+    if not any(char.isdigit() for char in password1):
+        return False
+
+    return True
+
+
+def _is_not_exists_email(email: str):
+    if CustomUser.objects.filter(email=email).exists():
+        return False
+    return True
