3aea9c1ab71f2e068be5a5692846509da77acdc2
Gasak Ruslan <rusya.gasak@mail.ru>
2023-08-03 14:17:01 +0300

    во views.py сделал авторзицию и регистрацию applicant (возможно нужно будет поменять регистрацию)

--- job_search_help_site/search_site/views.py
+++ job_search_help_site/search_site/views.py
@@ -1,4 +1,8 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
+
+from search_site.services import register
 
 
 def index(request):
@@ -10,12 +14,43 @@ def help_for_people(request):
 
 
 def login_applicant(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            error_message = "Неправильный email или пароль."
+            return render(request, 'login_applicant.html', {'error_message': error_message})
+
     return render(request, "login_applicant.html")
 
 
 def register_applicant(request):
     if request.method == "POST":
-        print("POST METHOD")
+        email = request.POST.get("email")
+        password1 = request.POST.get("password1")
+        password2 = request.POST.get("password2")
+
+        if not register.match_password(password1, password2):
+            messages.error(request, 'Пароли не совпадают')
+            return redirect('register_applicant')
+
+        if not register.is_valid_password(password1):
+            messages.error(request, "Пароль должен состоять из 8 символов и в нем должны быть буквы!")
+            return redirect('register_applicant')
+
+        if not register.is_not_exists_email(email):
+            messages.error(request, "Такой email уже зарегистрирован!")
+            return redirect("register_applicant")
+
+        register.register_applicant(email, password1)
+        user = authenticate(request, email=email, password=password1)
+        if user is not None:
+            login(request, user)
+
         return redirect("login_applicant")
 
     return render(request, "register_applicant.html")
@@ -31,3 +66,8 @@ def login_employer(request):
 
 def register_employer(request):
     return render(request, "register_employer.html")
+
+
+def logout_user(request):
+    logout(request)
+    return redirect("index")
