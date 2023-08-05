e66fd7257a6ca0702d15a9fd2c867d86a10eaa97
Gasak Ruslan <rusya.gasak@mail.ru>
2023-08-05 16:16:24 +0300

    в админке зарегал новые модельки

--- job_search_help_site/search_site/admin.py
+++ job_search_help_site/search_site/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
-from search_site.models import CustomUser
+from search_site import models
+
+
+"""
+ПОТОМ ЗАНЯТЬСЯ АДМИНКОЙ!!!!
+"""
 
 
 class CustomUserAdmin(admin.ModelAdmin):
@@ -9,4 +14,32 @@ class CustomUserAdmin(admin.ModelAdmin):
     search_fields = ("email", )
 
 
-admin.register(CustomUser, CustomUserAdmin)
+@admin.register(models.CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ("email", "is_active", "is_staff")
+    list_filter = ("is_active", "is_staff")
+    search_fields = ("email", )
+
+
+@admin.register(models.Applicant)
+class ApplicantAdmin(admin.ModelAdmin):
+    # Добавьте поля, которые хотите отображать для модели Applicant
+    pass
+
+
+@admin.register(models.Company)
+class CompanyAdmin(admin.ModelAdmin):
+    # Добавьте поля, которые хотите отображать для модели Company
+    pass
+
+
+@admin.register(models.Resume)
+class ResumeAdmin(admin.ModelAdmin):
+    # Добавьте поля, которые хотите отображать для модели Resume
+    pass
+
+
+@admin.register(models.Vacancy)
+class VacancyAdmin(admin.ModelAdmin):
+    # Добавьте поля, которые хотите отображать для модели Vacancy
+    pass
