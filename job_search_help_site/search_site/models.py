a1d35ac77d59179005ceed13d009d627b9051ca8
Gasak Ruslan <rusya.gasak@mail.ru>
2023-08-05 13:29:46 +0300

    написал новые модельки для БД

--- job_search_help_site/search_site/models.py
+++ job_search_help_site/search_site/models.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
+from django.core.validators import RegexValidator
 from django.db import models
+from django.utils import timezone
+
+phoneNumberRegex = RegexValidator(regex=r"^\+?1?\d{8,15}$")
 
 
 class CustomUserManager(BaseUserManager):
@@ -49,3 +53,108 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
         related_name='customuser_set',  # Уникальное имя обратной связи для разрешений
         related_query_name='customuser'
     )
+
+
+class Specialization(models.Model):
+    name_specialization = models.TextField()
+
+    class Meta:
+        verbose_name_plural = "Специализация"
+
+
+class Education(models.Model):
+    name_of_education = models.CharField(max_length=30, null=False)
+
+    class Meta:
+        verbose_name_plural = "Образование"
+
+
+class TypeEmployment(models.Model):
+    name_type_of_employment = models.CharField(max_length=30, null=False)
+
+    class Meta:
+        verbose_name_plural = "Тип занятости"
+
+
+class Applicant(models.Model):
+    """
+    Кандидат который будет подавать резюме компании
+    """
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    is_confirmed = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name_plural = "Кандитаты"
+
+
+class Resume(models.Model):
+    """
+    Резюме, которое будет подаваться компании кандидатом
+    """
+    # ТУТ СДЕЛАТЬ ТОЖЕ ЧТОБЫ МОЖНО БЫЛО ПОДНИМАТЬ РЕЗЮМЕ КАК В МОДЕЛИ Vacancy!!!!
+    name = models.CharField(max_length=30, null=False)
+    second_name = models.CharField(max_length=30, null=False)
+    last_name = models.CharField(max_length=30, null=False)
+    gender = models.CharField(max_length=10, verbose_name="Пол")
+    education = models.ForeignKey(Education, on_delete=models.SET_NULL, null=True, verbose_name="Образование")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Время обновления")
+
+    phone = models.CharField(max_length=16, validators=[phoneNumberRegex], unique=True)
+    image = models.ImageField(upload_to="", null=True)
+    profession = models.TextField()
+
+    key_skills = models.ForeignKey(Specialization, on_delete=models.CASCADE, verbose_name="Ключевые навыки")
+
+    applicant = models.ForeignKey(Applicant, on_delete=models.CASCADE, verbose_name="Кандидат")
+
+    class Meta:
+        verbose_name_plural = "Резюме"
+        ordering = ["-updated_at"]  # возможно нужно будет по другому !!!!
+
+
+class Company(models.Model):
+    """
+    Компания, которая будет публиковать вакансии
+    """
+    name_company = models.CharField(max_length=20, null=False, unique=True)
+    name_user = models.CharField(max_length=30, null=False)
+    second_name_user = models.CharField(max_length=30, null=False)
+    phone_company = models.CharField(max_length=16, validators=[phoneNumberRegex], unique=True, null=True)
+    image_company = models.ImageField(upload_to="", null=True)
+    is_confirmed = models.BooleanField(default=False)
+
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = "Компании"
+
+
+class Vacancy(models.Model):
+    """
+    Вакансии, которые будут подаваться компаниями
+    """
+    name_vacancy = models.CharField(max_length=50, null=False)
+    town = models.CharField(max_length=30, null=False)
+    salary = models.CharField(max_length=30)
+    experience = models.CharField(max_length=30)
+    description = models.CharField(max_length=300, verbose_name="Описание вакансии")
+    created_at = models.DateTimeField(auto_now_add=True)
+    publication_time = models.DateTimeField(default=timezone.now)  # либо использовать default=timezone.now
+    location = models.CharField(max_length=100)
+
+    type_of_employment = models.ForeignKey(TypeEmployment, on_delete=models.SET_NULL, null=True)
+    specialization = models.ForeignKey(Specialization, on_delete=models.SET_NULL, null=True)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = "Вакасии"
+        ordering = ["-publication_time"]
+
+
+class Application(models.Model):
+    company = models.CharField(max_length=100, default="")
+    applicant = models.ForeignKey(Applicant, on_delete=models.CASCADE)
+    vacancy = models.ForeignKey(Vacancy, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = "Отправка резюме"
