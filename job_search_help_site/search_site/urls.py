cc135a8f6225bc70908f3e817f9660b7c59ff1bb
Gasak Ruslan <rusya.gasak@mail.ru>
2023-08-14 14:25:59 +0300

    добавил ссылочку на страницу создания резюме в urls.py

--- job_search_help_site/search_site/urls.py
+++ job_search_help_site/search_site/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
     path("applicant/resumes", views.resumes_applicant, name="rezume_applicant"),
     path("applicant/home_page", views.applicant_home_page, name="applicant_home_page"),
     path("applicant/home_page/change_password", views.change_password, name="change_password_applicant"),
+    path("applicant/create_resume", views.create_resume, name="create_resume"),
 
     # url для employer
     path("employer/", views.index_employer, name="employer"),
