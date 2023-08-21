02c5f7f69d0dddcfad2227755af406f3bb3bd561
Gasak Ruslan <rusya.gasak@mail.ru>
2023-08-21 12:16:51 +0300

    Добавил функции для удаления и изменения видимости вакансии.

--- job_search_help_site/search_site/services/vacancy.py
+++ job_search_help_site/search_site/services/vacancy.py
@@ -1,11 +1,36 @@
 from django.core.exceptions import ValidationError
+from django.http import HttpResponseForbidden
+from django.shortcuts import get_object_or_404
 
 from . import validators
 from search_site import models
 from .home_page import get_company
 
 
-def create_vacancy_company(user: models.CustomUser, vacancy_data: dict):
+def _check_and_get_vacancy(user: models.CustomUser, vacancy_id: int) -> models.Vacancy | None:
+    """
+    Проверяет и возвращает vacancy.
+    """
+    vacancy = get_object_or_404(models.Vacancy, id=vacancy_id)
+    if not _check_is_company(user, vacancy):
+        return None
+    return vacancy
+
+
+def _check_is_company(user: models.CustomUser, vacancy: models.Vacancy) -> bool:
+    """
+    Проверка на то, что изменяет видимость vacancy тот, кто ее создал.
+    """
+    if vacancy.company != get_company(user):
+        return False
+    return True
+
+
+def get_all_vacancy_company(user: models.CustomUser) -> list[models.Vacancy]:
+    return models.Vacancy.objects.filter(company=(get_company(user)).id)
+
+
+def create_vacancy_company(user: models.CustomUser, vacancy_data: dict) -> bool:
     """
     Создается вакансия компании.
     """
@@ -30,5 +55,22 @@ def create_vacancy_company(user: models.CustomUser, vacancy_data: dict):
         return False
 
 
-def get_all_vacancy_company(user: models.CustomUser) -> list[models.Vacancy]:
-    return models.Vacancy.objects.filter(company=(get_company(user)).id)
+def change_published_vacancy(user: models.CustomUser, vacancy_id: int) -> HttpResponseForbidden | None:
+    """
+    Изменяет видимость вакансии комнпаии.
+    """
+    vacancy = _check_and_get_vacancy(user, vacancy_id)
+    if vacancy is None:
+        return HttpResponseForbidden("Упс, эта страница не доступна!")
+    vacancy.is_published = not vacancy.is_published
+    vacancy.save()
+
+
+def delete_vacancy(user: models.CustomUser, vacancy_id: int) -> HttpResponseForbidden | None:
+    """
+    Удаляет vacancy у  по vacancy_id.
+    """
+    vacancy = _check_and_get_vacancy(user, vacancy_id)
+    if vacancy is None:
+        return HttpResponseForbidden("Упс, эта страница не доступна!")
+    vacancy.delete()
