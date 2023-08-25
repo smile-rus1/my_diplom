bbaf9ce1e00a3ee79526d4ded46bf3071c744cc3
Gasak Ruslan <rusya.gasak@mail.ru>
2023-08-25 11:41:19 +0300

    Добавил проверку на то, есть ли у applicant хотя бы одно резюме, а также проверку на то, откликнулся ли уже applicant на эту вакансию

--- job_search_help_site/search_site/services/response_for_applicant.py
+++ job_search_help_site/search_site/services/response_for_applicant.py
@@ -1,22 +1,29 @@
 from search_site import models
 
 
-def respond_on_vacancy(user: models.CustomUser, vacancy_id: int, cover_letter: str):
+def respond_on_vacancy(user: models.CustomUser, vacancy_id: int, cover_letter: str) -> models.Application | None:
     """
     Applicant откликается на вакансию company.
     """
     applicant = models.Applicant.objects.get(user=user)
-    vacancy = models.Vacancy.objects.get(pk=vacancy_id)
+    if not _is_applicant_has_resume(applicant):
+        return None
 
-    models.Application.objects.create(
+    vacancy = models.Vacancy.objects.get(pk=vacancy_id)
+    application = models.Application.objects.create(
         applicant=applicant,
         vacancy=vacancy,
         cover_letter=cover_letter,
         company=vacancy.company.title_company
     )
+    return application
 
 
-def get_has_applied_respond(user: models.CustomUser, vacancy):
+def _is_applicant_has_resume(applicant: models.Applicant):
+    return models.Resume.objects.filter(applicant=applicant)
+
+
+def is_has_applied_respond(user: models.CustomUser, vacancy):
     return models.Application.objects.filter(applicant=models.Applicant.objects.get(user=user),
                                              vacancy=vacancy).exists()
 
