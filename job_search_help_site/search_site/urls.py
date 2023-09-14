901097f8f8ea79061f3732e2a8d993795283677b
Gasak Ruslan <rusya.gasak@mail.ru>
2023-09-14 17:56:00 +0300

    Добавил к основным urls новый url для поиска кандидата.

--- job_search_help_site/search_site/urls.py
+++ job_search_help_site/search_site/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path("applicant/update_resume/<int:resume_id>", views.update_resume, name="update_resume"),
     path("vacancy/<int:vacancy_id>", views.vacancy_for_applicant, name="vacancy"),
     path("applicant/responses/", views.respond_on_vacancy_applicant, name="all_respond_on_vacancy"),
+    path("applicant/search/", views.search_vacancy, name="search_vacancy"),
 
     # url для employer
     path("employer/", views.index_employer, name="employer"),
