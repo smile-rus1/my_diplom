60e8ed4f5fc677efbca99bc23124a9b1d86b296b
Gasak Ruslan <rusya.gasak@mail.ru>
2023-09-22 18:21:39 +0300

    Поменял название функции и параметра.

--- job_search_help_site/search_site/pagination_for_pages.py
+++ job_search_help_site/search_site/pagination_for_pages.py
@@ -3,8 +3,8 @@ from django.core.paginator import Paginator
 from search_site import models
 
 
-def create_pagination_for_applicant_search(vacancies: models.Vacancy) -> Paginator:
+def create_pagination_for_search(data: models.Vacancy | models.Resume) -> Paginator:
     """
     Создает пагинатор для страницы поиска для кандидата.
     """
-    return Paginator(vacancies, 20)
+    return Paginator(data, 20)
