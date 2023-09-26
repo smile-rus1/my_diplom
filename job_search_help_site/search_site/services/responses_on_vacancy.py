b6c251ec998eab1dea3383a4b497790209ed9251
Gasak Ruslan <rusya.gasak@mail.ru>
2023-09-26 20:40:08 +0300

    Добавил сортировку по application_date, в будующем сделать, чтобы можно было выбирать сортировку.

--- job_search_help_site/search_site/services/responses_on_vacancy.py
+++ job_search_help_site/search_site/services/responses_on_vacancy.py
@@ -2,7 +2,8 @@ from search_site import models
 
 
 def get_all_responses_on_vacancy(user: models.CustomUser) -> models.Application:
-    return models.Application.objects.filter(applicant__user=user).prefetch_related("applicant")
+    return models.Application.objects.filter(applicant__user=user).prefetch_related("applicant")\
+        .order_by("-application_date")
 
 
 def get_filter_responses(response: models.Application, status: str) -> models.Application:
