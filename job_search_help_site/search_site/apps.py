eb0aa3e8fa54f36d6642197a6ef72805d7ce650b
Gasak Ruslan <rusya.gasak@mail.ru>
2023-09-29 18:01:46 +0300

    Добавил импорт сигналов.

--- job_search_help_site/search_site/apps.py
+++ job_search_help_site/search_site/apps.py
@@ -5,3 +5,6 @@ class SearchSiteConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'search_site'
     verbose_name = "Сайт поиска работы"
+
+    def ready(self):
+        import search_site.signals
