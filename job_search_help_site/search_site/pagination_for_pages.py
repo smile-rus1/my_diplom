fb840b814659ed15dbfff112edf5863a25658682
Gasak Ruslan <rusya.gasak@mail.ru>
2023-10-03 21:17:52 +0300

    Изменил тип параметра у функции.

--- job_search_help_site/search_site/pagination_for_pages.py
+++ job_search_help_site/search_site/pagination_for_pages.py
@@ -1,9 +1,9 @@
+from typing import Any
+
 from django.core.paginator import Paginator
 
-from search_site import models
 
-
-def create_pagination_for_search(data: models.Vacancy | models.Resume) -> Paginator:
+def create_pagination_for_search(data: Any) -> Paginator:
     """
     Создает пагинатор для страницы поиска для кандидата.
     """
