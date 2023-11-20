499456692665cf32c1053828f7d067ab307143fd
Gasak Ruslan <rusya.gasak@mail.ru>
2023-11-20 12:03:09 +0300

    Добавил новый модуль для обобщения отправки сообщения на email.

---
+++ job_search_help_site/search_site/services/send_mail_to_users.py
@@ -0,0 +1,18 @@
+from typing import Any
+
+from django.core.mail import send_mail
+
+from job_search_help_site import settings
+
+
+def send_mail_to_users(**data: Any) -> None:
+    """
+    Присылает сообщение кандидату, о приглашении компании на вакансию.
+    """
+    send_mail(
+        subject=f"{data.get('subject')}",
+        message=f"{data.get('message')}",
+        from_email=settings.EMAIL_HOST_USER,
+        recipient_list=[data.get("email_recipient"), ],
+        fail_silently=False
+    )
