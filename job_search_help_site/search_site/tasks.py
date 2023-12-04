bed25216d907fdf73ed0cbd99042fd1ad5875fab
Gasak Ruslan <rusya.gasak@mail.ru>
2023-12-04 15:04:14 +0300

    Добавил функцию для обобщенной таски send_message_on_email, она будет отправлять различные сообщения.

--- job_search_help_site/search_site/tasks.py
+++ job_search_help_site/search_site/tasks.py
@@ -23,4 +23,18 @@ def send_link_confirmation_on_register(data_user: dict[str, Any]) -> None:
 
 @app.task
 def send_link_from_forgot_password_page(mail_data: dict[str, str]) -> None:
+    """
+    Присылает сообщение на почту с созданием нового пароля.
+    """
     messages.send_message_with_link_from_forgot_password_page(mail_data)
+
+
+@app.task
+def send_message_on_email(data: dict[str, Any]) -> None:
+    """
+    Присылает сообщение на email кандидату/компании.
+
+    data: Any data
+    send_mail_to_users: function send any mail to users.
+    """
+    messages.send_mail_to_users(data)
