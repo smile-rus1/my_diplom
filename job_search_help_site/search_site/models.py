fa8950cdcce3b67c3480ff16d32a13a3c011cf07
Gasak Ruslan <rusya.gasak@mail.ru>
2024-02-14 09:59:30 +0300

    Немного поменял верстку, добавил кнопку для добавления вакансии в "избранное".

--- job_search_help_site/search_site/models.py
+++ job_search_help_site/search_site/models.py
@@ -181,6 +181,17 @@ class LikeVacancyUser(models.Model):
         unique_together = ('user', 'vacancy')
 
 
+# class LikeResumeUser(models.Model):
+#     """
+#
+#     """
+#     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+#     resume = models.ForeignKey(Resume, on_delete=models.CASCADE)
+#
+#     class Meta:
+#         verbose_name_plural = ""
+
+
 class RequestToVerificationUser(models.Model):
     """
     Model to verification user role.
